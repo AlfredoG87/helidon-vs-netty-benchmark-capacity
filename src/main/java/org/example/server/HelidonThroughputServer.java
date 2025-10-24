8532a8311ca5fabaf80de301bf3a21a55eeec695
Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>
2025-10-24 15:02:15 -0400

    Tuning options.
    Signed-off-by: Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>

--- src/main/java/org/example/server/HelidonThroughputServer.java
+++ src/main/java/org/example/server/HelidonThroughputServer.java
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.example.server;
 
-import io.grpc.ServerServiceDefinition;
-import io.helidon.webserver.WebServer;
-import io.helidon.webserver.grpc.GrpcRouting;
-import org.example.throughput.ThroughputServiceGrpc;
-
 import java.time.Duration;
+
+import io.helidon.webserver.WebServer;
+import io.helidon.webserver.grpc.GrpcConfig;
+import io.helidon.webserver.grpc.GrpcRouting;
+
+import io.grpc.ServerServiceDefinition;
+import org.example.throughput.ThroughputServiceGrpc;
 
 public final class HelidonThroughputServer implements ThroughputServer {
     private final int port;
@@ -22,6 +24,13 @@ public final class HelidonThroughputServer implements ThroughputServer {
         GrpcRouting.Builder grpc = GrpcRouting.builder().service(ssd);
         server = WebServer.builder()
                 .port(port)
+                .connectionOptions(b -> b.tcpNoDelay(true))
+                .backlog(8 * 1024)
+                .writeQueueLength(8 * 1024)
+                .addProtocol(GrpcConfig.builder()
+                                     .enableCompression(false)
+                                     .enableMetrics(false)
+                                     .build())
                 .addRouting(grpc)
                 .build();
         server.start();
